885ac1a6619a330344fa487b3b090e625c015ded
finesse52 <finesse52@yandex.ru>
2025-05-05 18:55:59 +0300

    Made configs

--- src/main/java/finesse52/restjwtproject/controller/MainController.java
+++ src/main/java/finesse52/restjwtproject/controller/MainController.java
@@ -1,15 +1,29 @@
 package finesse52.restjwtproject.controller;
 
+import static finesse52.restjwtproject.utils.Constants.SECURITY_SWAGGER;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/main")
+@SecurityRequirement(name = SECURITY_SWAGGER)
 public class MainController {
-    @GetMapping("/insecurity")
-    public String getInsecurityPage() {
-        return "Insecurity Page";
+
+    @GetMapping("/unsecurity")
+    public String getUnsecurityPage() {
+        return "unsecurity page";
+    }
+
+    @GetMapping("/security")
+    public String getSecurityPage() {
+        return "security page";
+    }
+
+    @GetMapping("/admin")
+    public String getAdminPage() {
+        return "admin page";
     }
 }

28a0c4725cf198457c51d73144d833915b46e573
finesse52 <finesse52@yandex.ru>
2025-05-05 22:34:08 +0300

    added new repo

---
+++ src/main/java/finesse52/restjwtproject/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package finesse52.restjwtproject.repository;
+
+import finesse52.restjwtproject.domain.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
